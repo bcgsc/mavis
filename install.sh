dd05cff4d5d765031e7f8b5480ff413130645d67
Caralyn Reisle <creisle@bcgsc.ca>
2016-10-20 18:09:04 +0000

    fixed TSV version error
    git-svn-id: https://svn.bcgsc.ca/svn/SVIA/sv_compile/trunk@124320 ad02020f-2a26-4133-947e-1829317c1416

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
 svn export https://svn.bcgsc.ca/svn/SVIA/vocab/tags/v0.1.0/vocab.py vocab.py
-svn export https://svn.bcgsc.ca/svn/SVIA/TSV/tags/v0.2.3/TSV.py TSV.py
+svn export https://svn.bcgsc.ca/svn/SVIA/TSV/tags/v0.3.0/TSV.py TSV.py
+echo "generating the documentation"
+export PATH=/projects/tumour_char/analysis_scripts/python/centos06/anaconda3_v2.3.0/bin:$PATH
+
+cd docs
+bash create.sh
