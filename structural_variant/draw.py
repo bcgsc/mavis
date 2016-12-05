d6ae374e0c80c2f33a0baaf4b180f6836f4847bb
Caralyn Reisle <creisle@bcgsc.ca>
2016-12-05 23:41:30 +0000

    before trying to make all children relative to parent instead of absolutely positioned in the svg
    git-svn-id: https://svn.bcgsc.ca/svn/SVIA/sv_compile/trunk@125706 ad02020f-2a26-4133-947e-1829317c1416

--- structural_variant/draw.py
+++ structural_variant/draw.py
@@ -1,9 +1,18 @@
 from structural_variant.annotate import Gene
-import svgwrite
+from svgwrite import Drawing
 from structural_variant.interval import Interval
+from structural_variant.constants import STRAND
 
 # draw gene level view
 # draw gene box
+
+GENE_FORWARD_SVG="""
+<g class='.gene'>
+    <path d='M{0} 0 L{1} {2}'>
+    </path>
+    <rect>
+    </rect>
+</g>"""
 
 
 class Diagram:
@@ -16,40 +25,46 @@ class Diagram:
         GENE2_COLOR_SELECTED='#518DC5',
         GENE1_COLOR='#325556',
         GENE2_COLOR='#657E91',
-        GENE_LABEL_FILL='#FFFFFF',
-        GENE_LABEL_STROKE='#000000',
+        LABEL_FILL='#FFFFFF',
+        LABEL_STROKE='#000000',
         TRANSCRIPT1_COLOR='#4C9677',
         TRANSCRIPT2_COLOR='#518DC5',
         TRANSCRIPT1_UTR_COLOR='#7bddc1',
         TRANSCRIPT2_UTR_COLOR='#7dc3d8',
         DOMAIN_COLOR='#b8d3ba',
         WIDTH=1000,
+        PADDING=5,
+        GENE_MIN_BUFFER=200
     ):
         self.GENE1_COLOR_SELECTED = GENE1_COLOR_SELECTED
         self.GENE2_COLOR_SELECTED = GENE2_COLOR_SELECTED
         self.GENE1_COLOR = GENE1_COLOR
         self.GENE2_COLOR = GENE2_COLOR
-        self.GENE_LABEL_FILL = GENE_LABEL_FILL
-        self.GENE_LABEL_STROKE = GENE_LABEL_STROKE
+        self.LABEL_FILL = LABEL_FILL
+        self.LABEL_STROKE = LABEL_STROKE
         self.TRANSCRIPT1_COLOR = TRANSCRIPT1_COLOR
         self.TRANSCRIPT2_COLOR = TRANSCRIPT2_COLOR
         self.TRANSCRIPT1_UTR_COLOR = TRANSCRIPT1_UTR_COLOR
         self.TRANSCRIPT2_UTR_COLOR = TRANSCRIPT2_UTR_COLOR
         self.DOMAIN_COLOR = DOMAIN_COLOR
         self.TRACK_HEIGHT = 50
+        self.LINE_WIDTH = 4
+        self.LINE_COLOR = '#000000'
         self.DOMAIN_TRACK_HEIGHT = 20
         self.WIDTH = WIDTH
         self.GENE_INTERGENIC_RATIO = 10
         self.EXON_INTRON_RATIO = 10
         self.MIN_WIDTH = 2 # no element (exon, gene, etc can be less than this wide)
-        self.MIN_INTERGENIC_BUFFER = 200
+        self.GENE_MIN_BUFFER = GENE_MIN_BUFFER
         self.TRACK_LINE_HEIGHT = 4
         self.LEFT_MARGIN = 20
         self.RIGHT_MARGIN = 20
         self.TOP_MARGIN = 20
         self.BOTTOM_MARGIN = 20
         self.INNER_MARGIN = 20
-    
+        self.GENE_ARROW_WIDTH = 20
+        self.PADDING = PADDING
+
     def draw_legend(self):
         pass
 
@@ -57,6 +72,8 @@ class Diagram:
         # decide one or two gene tracks
         left_gene_track = None
         right_gene_track = None
+
+        canvas = Drawing(height=100, width=self.WIDTH) # just set the height for now and change later
 
         if ann.interchromosomal:  # two gene tracks
             pass
@@ -69,70 +86,211 @@ class Diagram:
                 genes.add(ann.transcript1.gene)
             if ann.transcript2 and hasattr(ann.transcript2, 'gene'):
                 genes.add(ann.transcript2.gene)
-            
-            gene_track, mapping = self._draw_gene_track(w, genes)
+
+
+            # draw the genes in order
+
 
         # add transcript tracks if applicable (and domains)
         # add fusion track if applicable
 
+    def draw_transcript(self, canvas, target_width, t, exon_color, utr_color):
+        main_group = canvas.g()
+        mapping = self._generate_exon_mapping(target_width, t.exons)
+
+        main_group.add(
+            canvas.rect(
+                (0, self.TRACK_HEIGHT / 2 - self.LINE_WIDTH / 2),
+                (target_width, self.LINE_WIDTH),
+                fill=self.LINE_COLOR
+                ))
+        y = 0
+        # draw the exons
+        for exon in sorted(t.exons, key=lambda x: x.start):
+            s = Interval.convert_pos(mapping, exon.start)
+            t = Interval.convert_pos(mapping, exon.end)
+
+            group = canvas.g(class_='exon')
+            main_group.add(group)
+
+            group.add(
+                canvas.rect(
+                    (s, y),
+                    (t - s + 1, self.TRACK_HEIGHT),
+                    fill=exon_color
+                ))
+        #y += self.TRACK_HEIGHT + self.PADDING
+
+        # now draw the domain tracks
+        # need to convert the domain AA positions to cds positions to genomic
+
+        return main_group
+
+
+    def draw_gene_subdiagram(self, canvas, target_width, genes, breakpoint=None, colors={}):
+        main_group = canvas.g()
+        mapping = self._generate_gene_mapping(target_width, genes)
+        print('draw_gene_subdiagram', canvas, target_width, genes, colors)
+        gene_px_intervals = {}
+        for gene in genes:
+            s = Interval.convert_pos(mapping, gene.start)
+            t = Interval.convert_pos(mapping, gene.end)
+            gene_px_intervals[Interval(s, t)] = gene
+        tracks = Diagram._split_intervals_into_tracks(gene_px_intervals)
+
+        y = 0
+
+        main_group.add(
+            canvas.rect(
+                (0, self.TRACK_HEIGHT / 2 - self.LINE_WIDTH / 2 + (len(tracks) - 1) * (self.TRACK_HEIGHT + self.PADDING)),
+                (target_width, self.LINE_WIDTH),
+                fill=self.LINE_COLOR
+                ))
+        tracks.reverse()
+        for track in tracks:  # svg works from top down
+            for genepx in track:
+                # draw the gene
+                wrect = genepx.end - self.GENE_ARROW_WIDTH - genepx.start + 1
+                gene = gene_px_intervals[genepx]
+                group = canvas.g(class_='gene')
+                main_group.add(group)
+
+                if gene.strand == STRAND.POS:
+                    group.add(
+                        canvas.rect(
+                            (genepx.start, y),
+                            (wrect, self.TRACK_HEIGHT),
+                            fill=colors.get(gene, self.GENE1_COLOR)
+                        )
+                    )
+                    group.add(
+                        canvas.polyline(
+                            [
+                                (genepx.start + wrect, y),
+                                (genepx.start + wrect + self.GENE_ARROW_WIDTH, y + self.TRACK_HEIGHT / 2),
+                                (genepx.start + wrect, y + self.TRACK_HEIGHT)
+                            ],
+                            fill=colors.get(gene, self.GENE1_COLOR)
+                        )
+                    )
+                elif gene.strand == STRAND.NEG:
+                    group.add(
+                        canvas.rect(
+                            (genepx.start + self.GENE_ARROW_WIDTH, y),
+                            (wrect, self.TRACK_HEIGHT),
+                            fill=colors.get(gene, self.GENE1_COLOR)
+                        )
+                    )
+                    group.add(
+                        canvas.polyline(
+                            [
+                                (genepx.start + self.GENE_ARROW_WIDTH, y),
+                                (genepx.start, y + self.TRACK_HEIGHT / 2),
+                                (genepx.start + self.GENE_ARROW_WIDTH, y + self.TRACK_HEIGHT)
+                            ],
+                            fill=colors.get(gene, self.GENE1_COLOR)
+                        )
+                    )
+                else:
+                    raise AttributeError('gene must specify positive or negative strand to be drawn')
+            y += self.TRACK_HEIGHT + self.PADDING
+        return main_group
 
     def read_config(self):
         pass
-   
+
+    def _generate_exon_mapping(self, target_width, exons):
+        return self._generate_interval_mapping(
+            target_width,
+            exons,
+            self.EXON_INTRON_RATIO,
+            self.MIN_WIDTH
+        )
+
     def _generate_gene_mapping(self, target_width, genes):
+        return self._generate_interval_mapping(
+            target_width,
+            genes,
+            self.GENE_INTERGENIC_RATIO,
+            self.MIN_WIDTH + self.GENE_ARROW_WIDTH,
+            buffer=self.GENE_MIN_BUFFER
+        )
+
+    @classmethod
+    def _split_intervals_into_tracks(cls, intervals):
+        tracks = [[]]
+        for i in sorted(intervals, key=lambda x: x[0]):
+            added = False
+            for t in tracks:
+                overlaps = False
+                for og in t:
+                    if Interval.overlaps(i, og):
+                        overlaps = True
+                        break
+                if not overlaps:
+                    added = True
+                    t.append(i)
+                    break
+            if not added:
+                tracks.append([i])
+        return tracks
+
+    @classmethod
+    def _generate_interval_mapping(cls, target_width, input_intervals, ratio, min_width, buffer=0):
         intervals = []
-        
-        for i in Interval.min_nonoverlapping(*genes):
+
+        for i in Interval.min_nonoverlapping(*input_intervals):
             if len(intervals) == 0 or abs(Interval.dist(intervals[-1], i)) > 1:
                 intervals.append(i)
             else:
                 intervals[-1] = intervals[-1] | i
-        print(intervals)
-        start = intervals[0].start - self.MIN_INTERGENIC_BUFFER
-        end = intervals[1].end + self.MIN_INTERGENIC_BUFFER
+
+        start = intervals[0].start - buffer
+        end = intervals[1].end + buffer
         genic_length = sum([len(i) for i in intervals])
         intergenic_length = end - start + 1 - genic_length
-        
-        width = target_width - (len(intervals) * 2 + 1) * self.MIN_WIDTH  # reserved width
+
+        width = target_width - (len(intervals) * 2 + 1) * min_width  # reserved width
 
         if width < 0:
             raise AttributeError('width cannot accommodate the number of expected objects')
 
-        intergenic_unit = width / (genic_length * self.GENE_INTERGENIC_RATIO + intergenic_length)
-        genic_unit = intergenic_unit * self.GENE_INTERGENIC_RATIO
+        intergenic_unit = width / (genic_length * ratio + intergenic_length)
+        genic_unit = intergenic_unit * ratio
 
-        mapping = {}
-        
+        mapping = []
+
         pos = 1
         # do the intergenic region prior to the first genic region
-        ifrom = Interval(start, intervals[0].start - 1)
-        ito = Interval(pos, pos + self.MIN_WIDTH - 1 + max(len(ifrom) * intergenic_unit - 1, 0))
-        mapping[ifrom] = ito
-        pos += len(ito)
+        if buffer > 0:
+            ifrom = Interval(start, intervals[0].start - 1)
+            ito = Interval(pos, pos + min_width - 1 + max(len(ifrom) * intergenic_unit - 1, 0))
+            mapping.append((ifrom, ito))
+            pos += len(ito)
 
         for i, curr in enumerate(intervals):
-            print('curr', curr)
             if i > 0:
                 prev = intervals[i - 1]
-                print('prev', prev)
                 ifrom = Interval(prev.end + 1, curr.start - 1)
-                ito = Interval(pos, pos + self.MIN_WIDTH - 1 + max(len(ifrom) * intergenic_unit - 1, 0))
-                mapping[ifrom] = ito
+                ito = Interval(pos, pos + min_width - 1 + max(len(ifrom) * intergenic_unit - 1, 0))
+                mapping.append((ifrom, ito))
                 pos += len(ito)
-            
-            ito = Interval(pos, pos + self.MIN_WIDTH - 1 + max(len(curr) * genic_unit - 1, 0))
-            mapping[curr] = ito
+
+            ito = Interval(pos, pos + min_width - 1 + max(len(curr) * genic_unit - 1, 0))
+            mapping.append((curr, ito))
             pos += len(ito)
-        
+
         # now the last intergenic region will make up for the rounding error
-        ifrom = Interval(intervals[-1].end + 1, end)
-        ito = Interval(pos, target_width - 1)
-        mapping[ifrom] = ito
-        pos += len(ito)
-        print(sorted(mapping.items()))
-        print('pos', pos)
-        print('target_width', target_width)
-        self.assertTrue(False)
+        if buffer > 0:
+            ifrom = Interval(intervals[-1].end + 1, end)
+            ito = Interval(pos, pos + min_width - 1 + max(len(ifrom) * intergenic_unit - 1, 0))
+            mapping.append((ifrom, ito))
+            pos += len(ito)
+
+        mapping[-1][1].end = target_width - 1
+
+        temp = mapping
+        mapping = dict()
+        for ifrom, ito in temp:
+            mapping[ifrom] = ito
         return mapping
-
-
--- tests/test_draw.py
+++ tests/test_draw.py
@@ -1,10 +1,65 @@
 import unittest
+import re
 from structural_variant.draw import Diagram
-from structural_variant.annotate import Gene
+from structural_variant.annotate import Gene, Transcript
+from svgwrite import Drawing
+from structural_variant.constants import STRAND
+from structural_variant.interval import Interval
+
 
 class TestDraw(unittest.TestCase):
-    def test__draw_gene_track(self):
-        # don't actually need to use genes here
+    def test__generate_gene_mapping(self):
         d = Diagram()
-        genes = [Gene('1', 1000, 2000), Gene('1', 5000, 7000), Gene('1', 1500, 2500)]
-        d._generate_gene_mapping(100, genes)
+        a = Gene('1', 1000, 2000)
+        b = Gene('1', 5000, 7000)
+        c = Gene('1', 1500, 2500)
+        genes = [a, b, c]
+        with self.assertRaises(AttributeError):
+            m = d._generate_gene_mapping(100, genes)
+        m = d._generate_gene_mapping(500, genes)
+        u = Interval.union(*m.values())
+        self.assertLessEqual(1, u.start)
+        self.assertGreaterEqual(500, u.end)
+
+    def test__split_intervals_into_tracks(self):
+        # ----======---------
+        # ------======--------
+        # -----===============
+        t = Diagram._split_intervals_into_tracks(
+            [(1, 3), (3, 7), (2, 2), (4, 5), (3, 10)]
+        )
+        self.assertEqual(3, len(t))
+        self.assertEqual([(1, 3), (4, 5)], t[0])
+        self.assertEqual([(2, 2), (3, 7)], t[1])
+        self.assertEqual([(3, 10)], t[2])
+
+    def test_draw_gene_subdiagram(self):
+        canvas = Drawing(height=100, width=1000)
+        d = Diagram(
+            GENE_MIN_BUFFER=200,
+            PADDING=5,
+            GENE1_COLOR='#325556',
+        )
+        genes = [
+            Gene('1', 1000, 2000, strand=STRAND.POS),
+            Gene('1', 5000, 7000, strand=STRAND.NEG),
+            Gene('1', 1500, 2500, strand=STRAND.POS)
+        ]
+        g = d.draw_gene_subdiagram(canvas, 500, genes)
+        canvas.add(g)
+        canvas.saveas('test.svg')
+
+    def test_draw_transcript(self):
+        canvas = Drawing(height=100, width=1000)
+        d = Diagram(
+            GENE_MIN_BUFFER=200,
+            PADDING=5,
+            GENE1_COLOR='#325556',
+        )
+        t = Transcript(gene=None, cds_start=50, cds_end=249, exons=[(1, 99), (200, 299), (400, 499)], strand=STRAND.POS)
+        g = d.draw_transcript(canvas, 500, t, exon_color='#FF0000', utr_color='#FFFF00')
+        print(g)
+        canvas.add(g)
+        print(canvas.tostring())
+        #canvas.saveas('test.svg')
+        self.assertFalse(True)
