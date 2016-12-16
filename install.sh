6e93985e173e151d71e0390f780cc711e161c9e5
Caralyn Reisle <creisle@bcgsc.ca>
2016-12-16 20:57:09 +0000

    fixed dependency install
    git-svn-id: https://svn.bcgsc.ca/svn/SVIA/svmerge/trunk@125955 ad02020f-2a26-4133-947e-1829317c1416

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 svn export https://svn.bcgsc.ca/svn/SVIA/vocab/tags/v0.1.0/vocab.py vocab.py
-svn export https://svn.bcgsc.ca/svn/SVIA/TSV/tags/v2.0.0/TSV.py TSV.py
+svn export https://svn.bcgsc.ca/svn/SVIA/TSV/tags/v2.0.1/TSV.py TSV.py
 echo "generating the documentation"
 export PATH=/projects/tumour_char/analysis_scripts/python/centos06/anaconda3_v2.3.0/bin:$PATH
 
