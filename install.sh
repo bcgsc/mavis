dcfc7c68b379c35cbdb0ba2dddb2e9672f872357
Caralyn Reisle <creisle@bcgsc.ca>
2017-01-05 23:56:18 +0000

    added svn modules to req file
    git-svn-id: https://svn.bcgsc.ca/svn/SVIA/svmerge/trunk@126198 ad02020f-2a26-4133-947e-1829317c1416

--- install.sh
+++
@@ -1,7 +0,0 @@
-svn export https://svn.bcgsc.ca/svn/SVIA/vocab/tags/v0.2.0/vocab.py vocab.py
-svn export https://svn.bcgsc.ca/svn/SVIA/TSV/tags/v2.0.1/TSV.py TSV.py
-echo "generating the documentation"
-export PATH=/projects/tumour_char/analysis_scripts/python/centos06/anaconda3_v2.3.0/bin:$PATH
-
-cd docs
-bash create.sh
