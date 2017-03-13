4472b92138b9ba71886df43f79f001b6b9ac39c9
Caralyn Reisle <creisle@bcgsc.ca>
2017-03-13 20:37:19 +0000

    added to glossary and documentation for sub-packages
    git-svn-id: https://svn.bcgsc.ca/svn/SVIA/svmerge/trunk@128210 ad02020f-2a26-4133-947e-1829317c1416

--- mavis/pairing/__init__.py
+++ mavis/pairing/__init__.py
@@ -14,9 +14,26 @@ Output Files
 | ``mavis_paired_*.tab``     | text/tabbed      |  call information and pairing information using product id |
 +----------------------------+------------------+------------------------------------------------------------+
 
+
 Algorithm Overview
 ---------------------
 
+- pairwise comparison of breakpoint pairs between libraries
+
+    - fail if orientations do not match
+    - fail if template/chromosomes do not match
+    - if the protocols are mixed
+   
+        - pass if the fusion products match at the sequence level
+        - pass if the breakpoint predicted from the genome matches the transcriptome breakpoint
+
+    - if the protocols are the same
+
+        - pass if the breakpoints are co-located
+
+- filter matches based on annotations
+
+    - if both breakpoints have the same gene annotation, they must also both have the same transcript annotation
 
 """
 from .pairing import equivalent_events
