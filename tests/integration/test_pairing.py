a4ab22e9bf8c80d13b208adbbddb57333175db98
Caleb Choo <cchoo@bcgsc.ca>
2017-05-09 14:30:25 -0700

    adding some pairing tests

--- tests/integration/test_pairing.py
+++ tests/integration/test_pairing.py
@@ -51,7 +51,7 @@ class TestPairing(unittest.TestCase):
     def test_genome_protocol_diff_chrom(self):
         self.gev2.break1.chr = '2'
         self.assertFalse(equivalent_events(self.gev1, self.gev2, self.TRANSCRIPTS))
-        raise unittest.SkipTest('TODO')
+
 
     def test_genome_protocol_diff_orient(self):
         self.gev2.break1.orient = ORIENT.LEFT
@@ -74,16 +74,57 @@ class TestPairing(unittest.TestCase):
         self.assertTrue(equivalent_events(self.gev1, self.gev2, self.TRANSCRIPTS))
 
     def test_genome_protocol_by_contig(self):
-        raise unittest.SkipTest('TODO')
+        self.gev1.break1_call_method = CALL_METHOD.CONTIG
+        self.gev2.break1_call_method = CALL_METHOD.CONTIG
+        self.DISTANCES[CALL_METHOD.CONTIG] = 0
+        self.DISTANCES[CALL_METHOD.SPLIT] = 10
+        self.assertTrue(equivalent_events(self.gev1, self.gev2, self.TRANSCRIPTS, DISTANCES=self.DISTANCES))
+
+        self.gev1.break1.start = 2
+        self.gev1.break1.end = 20
+        self.assertFalse(equivalent_events(self.gev1, self.gev2, self.TRANSCRIPTS, DISTANCES=self.DISTANCES))
 
     def test_genome_protocol_by_split(self):
-        raise unittest.SkipTest('TODO')
+        self.gev1.break1_call_method = CALL_METHOD.SPLIT
+        self.gev2.break1_call_method = CALL_METHOD.SPLIT
+        self.assertTrue(equivalent_events(self.gev1, self.gev2, self.TRANSCRIPTS, DISTANCES=self.DISTANCES))
+        self.DISTANCES[CALL_METHOD.FLANK] = 100
+        self.DISTANCES[CALL_METHOD.SPLIT] = 10
+        self.gev1.break1.start = 11
+        self.gev1.break1.end = 20
+        self.assertFalse(equivalent_events(self.gev1, self.gev2, self.TRANSCRIPTS, DISTANCES=self.DISTANCES))
 
     def test_genome_protocol_by_flanking(self):
-        raise unittest.SkipTest('TODO')
+        self.gev1.break1_call_method = CALL_METHOD.FLANK
+        self.gev2.break1_call_method = CALL_METHOD.FLANK
+        self.assertTrue(equivalent_events(self.gev1, self.gev2, self.TRANSCRIPTS, DISTANCES=self.DISTANCES))
+        self.DISTANCES[CALL_METHOD.FLANK] = 10
+        self.DISTANCES[CALL_METHOD.SPLIT] = 100
+        self.gev1.break1.start = 11
+        self.gev1.break1.end = 20
+        self.assertFalse(equivalent_events(self.gev1, self.gev2, self.TRANSCRIPTS, DISTANCES=self.DISTANCES))
 
     def test_genome_protocol_by_mixed_call_methods(self):
-        raise unittest.SkipTest('TODO')
+        self.gev1.break1_call_method = CALL_METHOD.CONTIG
+        self.gev2.break1_call_method = CALL_METHOD.CONTIG
+        self.gev1.break2_call_method = CALL_METHOD.FLANK
+        self.gev2.break2_call_method = CALL_METHOD.SPLIT
+        self.DISTANCES[CALL_METHOD.CONTIG] = 0
+        self.DISTANCES[CALL_METHOD.SPLIT] = 10
+        self.assertTrue(equivalent_events(self.gev1, self.gev2, self.TRANSCRIPTS, DISTANCES=self.DISTANCES))
+
+        self.gev1.break2.start = 3
+        self.gev1.break2.end = 20
+        self.assertTrue(equivalent_events(self.gev1, self.gev2, self.TRANSCRIPTS, DISTANCES=self.DISTANCES))
+
+        self.gev1.break1_call_method = CALL_METHOD.CONTIG
+        self.gev2.break1_call_method = CALL_METHOD.SPLIT
+        self.gev2.break2_call_method = CALL_METHOD.CONTIG
+        self.gev1.break2_call_method = CALL_METHOD.SPLIT
+
+        self.DISTANCES[CALL_METHOD.CONTIG] = 0
+        self.DISTANCES[CALL_METHOD.SPLIT] = 2
+        self.assertTrue(equivalent_events(self.gev1, self.gev2, self.TRANSCRIPTS, DISTANCES=self.DISTANCES))
 
     def test_mixed_protocol_fusions_same_sequence(self):
         product_sequences = {'a': 'AATG', 'b': 'AATG'}
@@ -123,7 +164,7 @@ class TestPairing(unittest.TestCase):
         genome_ev.data[COLUMNS.fusion_sequence_fasta_id] = 'a'
         trans_ev.data[COLUMNS.fusion_sequence_fasta_id] = 'b'
         self.assertTrue(equivalent_events(genome_ev, trans_ev, self.TRANSCRIPTS, product_sequences=product_sequences))
-    
+
     def test_mixed_protocol_fusions_same_sequence_diff_translation(self):
         product_sequences = {'a': 'AATG', 'b': 'AATG'}
         genome_ev = BreakpointPair(
@@ -227,23 +268,61 @@ class TestPairing(unittest.TestCase):
         )
         self.assertTrue(equivalent_events(genome_ev, trans_ev, self.TRANSCRIPTS))
         self.assertTrue(equivalent_events(trans_ev, genome_ev, self.TRANSCRIPTS))
-        
+
         genome_ev.data[COLUMNS.transcript2] = self.ust1.name
         genome_ev.data[COLUMNS.transcript1] = None
         trans_ev.data[COLUMNS.transcript2] = self.ust1.name
         trans_ev.data[COLUMNS.transcript1] = None
         self.assertTrue(equivalent_events(genome_ev, trans_ev, self.TRANSCRIPTS))
         self.assertTrue(equivalent_events(trans_ev, genome_ev, self.TRANSCRIPTS))
-    
+
     def test_mixed_protocol_one_predicted_one_mismatch(self):
-        pass
+        genome_ev = BreakpointPair(
+            Breakpoint('1', 350, orient=ORIENT.LEFT),
+            Breakpoint('1', 400, orient=ORIENT.RIGHT),
+            opposing_strands=False,
+            data={
+                COLUMNS.event_type: SVTYPE.DEL,
+                COLUMNS.break1_call_method: CALL_METHOD.CONTIG,
+                COLUMNS.break2_call_method: CALL_METHOD.CONTIG,
+                COLUMNS.fusion_sequence_fasta_id: None,
+                COLUMNS.protocol: PROTOCOL.GENOME,
+                COLUMNS.transcript1: self.ust1.name,
+                COLUMNS.transcript2: None
+            }
+        )
+        trans_ev = BreakpointPair(
+            Breakpoint('1', 350, orient=ORIENT.LEFT),
+            Breakpoint('1', 400, orient=ORIENT.RIGHT),
+            opposing_strands=False,
+            data={
+                COLUMNS.event_type: SVTYPE.DEL,
+                COLUMNS.break1_call_method: CALL_METHOD.CONTIG,
+                COLUMNS.break2_call_method: CALL_METHOD.CONTIG,
+                COLUMNS.fusion_sequence_fasta_id: None,
+                COLUMNS.protocol: PROTOCOL.TRANS,
+                COLUMNS.transcript1: self.ust1.name,
+                COLUMNS.transcript2: None
+            }
+        )
+        self.assertTrue(equivalent_events(genome_ev, trans_ev, self.TRANSCRIPTS))
+        self.assertTrue(equivalent_events(trans_ev, genome_ev, self.TRANSCRIPTS))
+
+        genome_ev.data[COLUMNS.transcript2] = self.ust1.name
+        genome_ev.data[COLUMNS.transcript1] = None
+        trans_ev.data[COLUMNS.transcript2] = self.ust1.name
+        trans_ev.data[COLUMNS.transcript1] = None
+        self.assertTrue(equivalent_events(genome_ev, trans_ev, self.TRANSCRIPTS))
+        self.assertTrue(equivalent_events(trans_ev, genome_ev, self.TRANSCRIPTS))
+        
 
     def test_mixed_protocol_both_predicted(self):
+        
         raise unittest.SkipTest('TODO')
 
     def test_mixed_protocol_neither_predicted_one_match(self):
         raise unittest.SkipTest('TODO')
-    
+
     def test_mixed_protocol_neither_predicted_no_match(self):
         raise unittest.SkipTest('TODO')
 
@@ -257,6 +336,7 @@ class TestPairing(unittest.TestCase):
 class TestBreakpointPrediction(unittest.TestCase):
     def setUp(self):
         self.ust = usTranscript([Exon(101, 200), Exon(301, 400), Exon(501, 600)], strand=STRAND.POS)
+        self.n_ust = usTranscript([Exon(101, 200), Exon(301, 400), Exon(501, 600)], strand=STRAND.NEG)
 
     def test_exonic_five_prime(self):
         b = Breakpoint('1', 350, orient=ORIENT.LEFT)
@@ -295,3 +375,22 @@ class TestBreakpointPrediction(unittest.TestCase):
         breaks = predict_transcriptome_breakpoint(b, self.ust)
         self.assertEqual(1, len(breaks))
         self.assertEqual(301, breaks[0].start)
+
+    def test_outside_transcript(self):
+        b = Breakpoint('1', 100, orient=ORIENT.RIGHT)
+        with self.assertRaises(AssertionError):
+            breaks = predict_transcriptome_breakpoint(b, self.ust)
+
+    def test_exonic_five_prime_neg(self):
+        b = Breakpoint('1', 350, orient=ORIENT.LEFT, strand=STRAND.NEG)
+        breaks = predict_transcriptome_breakpoint(b, self.n_ust)
+        print(breaks)
+        self.assertEqual(2, len(breaks))
+        self.assertEqual(200, breaks[0].start)
+        self.assertEqual(b, breaks[1])
+
+    def test_intronic_five_prime_neg(self):
+        b = Breakpoint('1', 450, orient=ORIENT.LEFT, strand=STRAND.NEG)
+        breaks = predict_transcriptome_breakpoint(b, self.n_ust)
+        self.assertEqual(1, len(breaks))
+        self.assertEqual(400, breaks[0].start)
