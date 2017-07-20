0c576ccab6dca3d41025ebf7db43f77bc1fb8ee7
Caralyn Reisle <creisle@bcgsc.ca>
2017-07-20 13:43:34 -0700

    MAV-168: fixed bipython numpy order dependency problem by adding numpy to the setup_requires. fixed svn install hack by adding odules to path directly after pip.main

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 import subprocess
 import os
 import re
-
+import glob
 
 # install local svn dependencies
 cwd = os.path.dirname(os.path.abspath(__file__))
@@ -13,28 +13,39 @@ tsv_link = 'svn+https://svn.bcgsc.ca/svn/SVIA/TSV/tags/v{0}#egg=TSV-{0}'.format(
 vocab_version = '1.0.0'
 vocab_link = 'svn+https://svn.bcgsc.ca/svn/SVIA/vocab/tags/v{0}#egg=vocab-{0}'.format(vocab_version)
 
-if any([x in sys.argv for x in ['install', 'develop']]):
-    # install the svn dependencies. setuptools has tunnel error but pip can do this
+
+def reload_path_for_pip():
+    for d in glob.glob(os.path.join(pip.locations.src_prefix, '*')):
+        if d not in sys.path:
+            sys.path.append(d)
+
+
+def install_vocab():
     try:
         import vocab
         if vocab.__version__ != vocab_version:
             raise ImportError('wrong version', vocab.__version__)
-        print('Using', vocab.__path__[0])
-        print(vocab.__package__ + '==' + vocab.__version__)
-        print()
     except ImportError:
-        print(['install', '-e', vocab_link, '--trusted-host', '*.bcgsc.ca', '--exists-action', 's'])
         pip.main(['install', '-e', vocab_link, '--trusted-host', '*.bcgsc.ca', '--exists-action', 's'])
+        reload_path_for_pip()
+        import vocab
+    print('Using', vocab.__path__[0])
+    print(vocab.__package__ + '==' + vocab.__version__)
+    print()
+
+
+def install_TSV():
     try:
         import TSV
         if TSV.__version__ != tsv_version:
             raise ImportError('wrong version', TSV.__version__)
-        print('Using', TSV.__path__[0])
-        print(TSV.__package__ + '==' + TSV.__version__)
-        print()
     except ImportError:
-        print(['install', '-e', tsv_link, '--trusted-host', '*.bcgsc.ca', '--exists-action', 's'])
         pip.main(['install', '-e', tsv_link, '--trusted-host', '*.bcgsc.ca', '--exists-action', 's'])
+        reload_path_for_pip()
+        import TSV
+    print('Using', TSV.__path__[0])
+    print(TSV.__package__ + '==' + TSV.__version__)
+    print()
 
 
 def check_nonpython_dependencies():
@@ -71,17 +82,30 @@ def pull_version_from_git():
     m = re.match('^v?(\d+)\.(\d+)\.(\d+)-(\d+)-g\w+$', v)
     if not m:
         raise OSError('could not parse version number from git', v, '^v?(\d+)\.(\d+)\.(\d+)-\d+-g\d+$')
-    return '{}.{}.{}'.format(m.group(1), m.group(2), int(m.group(3)) + int(m.group(4)))
+    commit_number = int(m.group(4))
+    if commit_number != 0:
+        return '{}.{}.{}.{}'.format(m.group(1), m.group(2), m.group(3), commit_number)
+    else:
+        return '{}.{}.{}'.format(m.group(1), m.group(2), m.group(3))
 
 
-version = pull_version_from_git()
-print('version:', version)
-vfile = os.path.join(cwd, 'mavis', 'version.py')
-
-if any([x in sys.argv for x in ['install', 'develop', 'build']]):
+def write_version_file(version):
+    vfile = os.path.join(cwd, 'mavis', 'version.py')
     with open(vfile, 'w') as fh:
         print('writing version to:', vfile, version)
         fh.write('__version__ = \'{}\'\n'.format(version))
+
+version = pull_version_from_git()
+print('version:', version)
+
+
+if any([x in sys.argv for x in ['install', 'develop', 'build']]):
+    install_TSV()
+    install_vocab()
+    write_version_file(version)
+
+# HSTLIB is a dependency for pysam
+os.environ['HTSLIB_CONFIGURE_OPTIONS'] = '--disable-lzma'  # only required for CRAM files
 
 setup(
     name='mavis',
@@ -92,26 +116,31 @@ setup(
         'docutils <0.13.1',
         'colour',
         'networkx',
-        'biopython',
         'svgwrite',
-        'Sphinx',  # for building the documentation only
+        'sphinx',  # for building the documentation only
         'sphinx-rtd-theme',  # for building the documentation only
         'pysam>=0.9',
         'TSV=={}'.format(tsv_version),
         'vocab=={}'.format(vocab_version),
-        'numpy>=1.11.2',
+        'numpy>=1.13.1',
         'pyvcf==0.6.8',
-        'braceexpand==0.1.2'
+        'braceexpand==0.1.2',
+        'biopython>=1.70'
     ],
+    python_requires='>=3',
     author_email='creisle@bcgsc.ca',
     dependency_links=[
         vocab_link,
         tsv_version
     ],
-    setup_requires=['nose>=1.0'],
+    setup_requires=[
+        'nose>=1.0',
+        'numpy>=1.13.1',  # because biopython requires numpy to build but doesn't list it in setup_requires
+        'TSV=={}'.format(tsv_version),
+        'vocab=={}'.format(vocab_version)
+    ],
     test_suite='nose.collector',
     tests_require=['nose', 'timeout-decorator==0.3.3', 'coverage==4.2'],
     entry_points={'console_scripts': ['mavis = mavis.main:main']}
 )
-
 check_nonpython_dependencies()
