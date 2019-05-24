3322863d54aab91dc324902fcf96834b697be37c
Caralyn Reisle <creisle@bcgsc.ca>
2019-05-24 10:12:53 -0700

    lint with black

--- tests/integration/test_cluster.py
+++ tests/integration/test_cluster.py
@@ -20,14 +20,18 @@ class TestFullClustering(unittest.TestCase):
         # none of the 24 events in the mocked file should cluster together
         # if we change the mock file we may need to update this function
         bpps = []
-        for bpp in sorted(read_bpp_from_input_file(FULL_BASE_EVENTS), key=lambda x: (x.break1.chr, x.break2.chr)):
+        for bpp in sorted(
+            read_bpp_from_input_file(FULL_BASE_EVENTS), key=lambda x: (x.break1.chr, x.break2.chr)
+        ):
             if bpp.data[COLUMNS.protocol] == PROTOCOL.GENOME:
                 bpps.append(bpp)
                 print(bpp)
         self.assertEqual(28, len(bpps))
         clusters = merge_breakpoint_pairs(bpps, 10, 10)
 
-        for cluster, input_pairs in sorted(clusters.items(), key=lambda x: (x[1][0].break1.chr, x[1][0].break2.chr)):
+        for cluster, input_pairs in sorted(
+            clusters.items(), key=lambda x: (x[1][0].break1.chr, x[1][0].break2.chr)
+        ):
             print(cluster)
             for ip in input_pairs:
                 print('\t', ip)
@@ -37,7 +41,9 @@ class TestFullClustering(unittest.TestCase):
     def test_clustering_events(self):
         # this file contains 2 events that should be clustered and produce a valid bpp
         bpps = []
-        for bpp in sorted(read_bpp_from_input_file(CLUSTERED_EVENTS), key=lambda x: (x.break1.chr, x.break2.chr)):
+        for bpp in sorted(
+            read_bpp_from_input_file(CLUSTERED_EVENTS), key=lambda x: (x.break1.chr, x.break2.chr)
+        ):
             if bpp.data[COLUMNS.protocol] == PROTOCOL.GENOME:
                 bpps.append(bpp)
                 print(bpp)
@@ -46,7 +52,9 @@ class TestFullClustering(unittest.TestCase):
 
         self.assertEqual(1, len(clusters))
 
-        for cluster, input_pairs in sorted(clusters.items(), key=lambda x: (x[1][0].break1.chr, x[1][0].break2.chr)):
+        for cluster, input_pairs in sorted(
+            clusters.items(), key=lambda x: (x[1][0].break1.chr, x[1][0].break2.chr)
+        ):
             print(cluster)
             for ip in input_pairs:
                 print('\t', ip)
@@ -63,7 +71,6 @@ class TestFullClustering(unittest.TestCase):
 
 
 class TestMergeBreakpointPairs(unittest.TestCase):
-
     def test_order_is_retained(self):
         # BPP(Breakpoint(1:1925143-1925155R), Breakpoint(1:1925144L), opposing=False)
         # >>  BPP(Breakpoint(1:1925144L), Breakpoint(1:1925144-1925158R), opposing=False)
@@ -73,12 +80,14 @@ class TestMergeBreakpointPairs(unittest.TestCase):
                 Breakpoint('2', 1925144, 1925144, 'L'),
                 Breakpoint('2', 1925144, 1925158, 'R'),
                 event_type='deletion',
-                opposing_strands=False),
+                opposing_strands=False,
+            ),
             BreakpointPair(
                 Breakpoint('2', 1925143, 1925143, 'L'),
                 Breakpoint('2', 1925143, 1925158, 'R'),
                 event_type='deletion',
-                opposing_strands=False)
+                opposing_strands=False,
+            ),
         ]
         mapping = merge_breakpoint_pairs(pairs, 100, 25)
         for merge, inputs in mapping.items():
@@ -90,8 +99,16 @@ class TestMergeBreakpointPairs(unittest.TestCase):
         self.assertEqual('R', merge.break2.orient)
 
     def test_merging_identical_large_inputs(self):
-        b1 = BreakpointPair(Breakpoint(11, 12856838, 12897006, 'L'), Breakpoint(11, 12856838, 12897006, 'R'), opposing_strands=False)
-        b2 = BreakpointPair(Breakpoint(11, 12856838, 12897006, 'L'), Breakpoint(11, 12856838, 12897006, 'R'), opposing_strands=False)
+        b1 = BreakpointPair(
+            Breakpoint(11, 12856838, 12897006, 'L'),
+            Breakpoint(11, 12856838, 12897006, 'R'),
+            opposing_strands=False,
+        )
+        b2 = BreakpointPair(
+            Breakpoint(11, 12856838, 12897006, 'L'),
+            Breakpoint(11, 12856838, 12897006, 'R'),
+            opposing_strands=False,
+        )
         mapping = merge_breakpoint_pairs([b1, b2], 100, 25, verbose=True)
         self.assertEqual(1, len(mapping))
         merge = list(mapping)[0]
@@ -107,16 +124,36 @@ class TestMergeBreakpointPairs(unittest.TestCase):
 
     def test_events_separate(self):
         bpps = [
-            BreakpointPair(Breakpoint('4', 157002032, 157002046, orient='L'), Breakpoint('4', 157002343, 157002343, orient='R'), event_type=SVTYPE.DEL, untemplated_seq='', protocol='genome', tracking_id='manta-MantaDEL:55718:0:0:1:0:0'),
-            BreakpointPair(Breakpoint('4', 156935061, orient='L'), Breakpoint('4', 156935245, orient='R'), event_type=SVTYPE.DEL, untemplated_seq='', protocol='genome', tracking_id='delly-DEL00011007'),
-            BreakpointPair(Breakpoint('4', 157002046, orient='L'), Breakpoint('4', 157002358, orient='R'), event_type=SVTYPE.DEL, untemplated_seq='', protocol='genome', tracking_id='delly-DEL00011008')
+            BreakpointPair(
+                Breakpoint('4', 157002032, 157002046, orient='L'),
+                Breakpoint('4', 157002343, 157002343, orient='R'),
+                event_type=SVTYPE.DEL,
+                untemplated_seq='',
+                protocol='genome',
+                tracking_id='manta-MantaDEL:55718:0:0:1:0:0',
+            ),
+            BreakpointPair(
+                Breakpoint('4', 156935061, orient='L'),
+                Breakpoint('4', 156935245, orient='R'),
+                event_type=SVTYPE.DEL,
+                untemplated_seq='',
+                protocol='genome',
+                tracking_id='delly-DEL00011007',
+            ),
+            BreakpointPair(
+                Breakpoint('4', 157002046, orient='L'),
+                Breakpoint('4', 157002358, orient='R'),
+                event_type=SVTYPE.DEL,
+                untemplated_seq='',
+                protocol='genome',
+                tracking_id='delly-DEL00011008',
+            ),
         ]
         mapping = merge_breakpoint_pairs(bpps, 100, 25, verbose=True)
         self.assertEqual(2, len(mapping))
 
 
 class TestMergeIntervals(unittest.TestCase):
-
     def test_merge_even_length(self):
         i1 = Interval(1001, 1002)
         result = merge_integer_intervals(i1, i1, weight_adjustment=25)
