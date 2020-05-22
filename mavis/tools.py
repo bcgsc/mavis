65998e8d7fc928d67fec48bee1809ab394fd81d8
oneillkza <oneillkza@gmail.com>
2020-05-22 13:00:03 -0700

    Skip unrecognised SVTYPEs when parsing sniffles

--- mavis/tools.py
+++ mavis/tools.py
@@ -26,6 +26,7 @@ SUPPORTED_TOOL = MavisNamespace(
     CNVNATOR='cnvnator',
     STRELKA='strelka',
     STARFUSION='starfusion',
+    SNIFFLES='sniffles'
 )
 """
 Supported Tools used to call SVs and then used as input into MAVIS
@@ -60,6 +61,7 @@ TOOL_SVTYPE_MAPPING.update(
         'dup': [SVTYPE.DUP],
         'ITD': [SVTYPE.DUP],
         'IDP': [SVTYPE.INS],
+        'INVDUP': [SVTYPE.TRANS],  #treat Sniffles' invdup type like a translocation
     }
 )
 
@@ -325,6 +327,113 @@ def _parse_bnd_alt(alt):
         raise NotImplementedError('alt specification in unexpected format', alt)
 
 
+def _parse_sniffles_vcf(record, log=DEVNULL):
+    """
+    converts a vcf record from sniffles
+
+    Note:
+    Derived from _parse_vcf_record
+        CT = connection type, If given this field will be used in determining the orientation at the breakpoints.
+        From https://groups.google.com/forum/#!topic/delly-users/6Mq2juBraRY, we can expect certain CT types for
+        certain event types
+            - translocation/inverted translocation: 3to3, 3to5, 5to3, 5to5
+            - inversion: 3to3, 5to5
+            - deletion: 3to5
+            - duplication: 5to3
+    """
+    records = []
+    for alt in record.alts if record.alts else [None]:
+        info = {}
+        for key in record.info.keys():
+            try:
+                value = record.info[key]
+            except UnicodeDecodeError as err:
+                log('Ignoring invalid INFO field {} with error: {}'.format(key, err))
+            else:
+                try:
+                    value = value[0] if len(value) == 1 else value
+                except TypeError:
+                    pass  # anything non-tuple
+            info[key] = value
+
+        std_row = {}
+        if record.id and record.id != 'N':  # to account for NovoBreak N in the ID field
+            std_row['id'] = record.id
+
+        if info.get('SVTYPE', None) == 'BND':
+            chr2, end, orient2, ref, alt = _parse_bnd_alt(alt)
+            std_row[COLUMNS.break2_orientation] = orient2
+            std_row[COLUMNS.untemplated_seq] = alt
+            if record.ref != ref:
+                raise AssertionError(
+                    'Expected the ref specification in the vcf record to match the sequence '
+                    'in the alt string: {} vs {}'.format(record.ref, ref)
+                )
+        else:
+            chr2 = info.get('CHR2', record.chrom)
+            end = record.stop
+            if (
+                alt
+                and record.ref
+                and re.match(r'^[A-Z]+$', alt)
+                and re.match(r'^[A-Z]+', record.ref)
+            ):
+                std_row[COLUMNS.untemplated_seq] = alt[1:]
+                size = len(alt) - len(record.ref)
+                if size > 0:
+                    std_row[COLUMNS.event_type] = SVTYPE.INS
+                elif size < 0:
+                    std_row[COLUMNS.event_type] = SVTYPE.DEL
+        std_row.update({COLUMNS.break1_chromosome: record.chrom, COLUMNS.break2_chromosome: chr2})
+        if info.get(
+            'PRECISE', False
+        ):  # DELLY CI only apply when split reads were not used to refine the breakpoint which is then flagged
+            std_row.update(
+                {
+                    COLUMNS.break1_position_start: record.pos,
+                    COLUMNS.break1_position_end: record.pos,
+                    COLUMNS.break2_position_start: end,
+                    COLUMNS.break2_position_end: end,
+                }
+            )
+        else:
+            std_row.update(
+                {
+                    COLUMNS.break1_position_start: max(
+                        1, record.pos + info.get('CIPOS', (0, 0))[0]
+                    ),
+                    COLUMNS.break1_position_end: record.pos + info.get('CIPOS', (0, 0))[1],
+                    COLUMNS.break2_position_start: max(1, end + info.get('CIEND', (0, 0))[0]),
+                    COLUMNS.break2_position_end: end + info.get('CIEND', (0, 0))[1],
+                }
+            )
+
+        if 'SVTYPE' in info:
+            std_row[COLUMNS.event_type] = info['SVTYPE']
+
+
+
+        try:
+            orient1, orient2 = info['CT'].split('to')
+            connection_type = {'3': ORIENT.LEFT, '5': ORIENT.RIGHT, 'N': ORIENT.NS}
+            std_row[COLUMNS.break1_orientation] = connection_type[orient1]
+            std_row[COLUMNS.break2_orientation] = connection_type[orient2]
+        except KeyError:
+            pass
+        std_row.update(
+            {k: v for k, v in info.items() if k not in {'CHR2', 'SVTYPE', 'CIPOS', 'CIEND', 'CT'}}
+        )
+        # Only include this line if the SVTYPE is recognised
+        # This is to remove sniffles weird hybrid SVs eg DEL/INV
+        if(info['SVTYPE'] in TOOL_SVTYPE_MAPPING.keys()):
+            records.append(std_row)
+        else:
+            log('Ignoring record with unrecognised SVTYPE {}'.format(info['SVTYPE']))
+
+
+    return records
+
+
 def _parse_vcf_record(record, log=DEVNULL):
     """
     converts a vcf record
@@ -452,6 +561,7 @@ def _convert_tool_row(row, file_type, stranded, assume_no_untemplated=True):
         SUPPORTED_TOOL.VCF,
         SUPPORTED_TOOL.BREAKSEQ,
         SUPPORTED_TOOL.STRELKA,
+        SUPPORTED_TOOL.SNIFFLES,
     ]:
 
         std_row.update(row)
@@ -473,7 +583,7 @@ def _convert_tool_row(row, file_type, stranded, assume_no_untemplated=True):
         std_row[COLUMNS.break1_orientation] = (
             ORIENT.LEFT if row['genomic_strand1'] == STRAND.POS else ORIENT.RIGHT
         )
-        std_row[COLUMNS.break2_orientation] = (
+        std_row[COLUMNS.break2_orientation] =AttributeError: ('interval start > end is not allowed', 1, 0) (
             ORIENT.LEFT if row['genomic_strand2'] == STRAND.POS else ORIENT.RIGHT
         )
         std_row.update(
@@ -671,6 +781,24 @@ def _convert_tool_output(
         for vcf_record in vfile.fetch():
             try:
                 rows.extend(_parse_vcf_record(vcf_record, log=log))
+            except Exception as err:
+                if file_type != SUPPORTED_TOOL.STRELKA:
+                    raise err
+                else:
+                    log('Ignoring', vcf_record)
+
+    elif file_type == SUPPORTED_TOOL.SNIFFLES:
+        rows = []
+        vfile = VariantFile(input_file)
+        try:
+            vfile.header.info.add(
+                'END', number=1, type='Integer', description='End of the interval'
+            )
+        except ValueError:
+            pass
+        for vcf_record in vfile.fetch():
+            try:
+                rows.extend(_parse_sniffles_vcf(vcf_record, log=log))
             except Exception as err:
                 if file_type != SUPPORTED_TOOL.STRELKA:
                     raise err
