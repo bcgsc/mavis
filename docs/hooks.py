f88b6c4cd26e0f91b715507c2fa4c17871d9c84b
Caralyn Reisle <creisle@bcgsc.ca>
2021-04-20 17:02:52 -0700

    Update documentation

--- docs/hooks.py
+++ docs/hooks.py
@@ -1,71 +1,78 @@
+import json
 import os
 import re
+from textwrap import dedent
 
 from markdown_refdocs.main import extract_to_markdown
-from mavis.annotate.constants import DEFAULTS as ANNOTATION_DEFAULTS
-from mavis.cluster.constants import DEFAULTS as CLUSTER_DEFAULTS
-from mavis.config import REFERENCE_DEFAULTS
-from mavis.illustrate.constants import DEFAULTS as ILLUSTRATION_DEFAULTS
-from mavis.pairing.constants import DEFAULTS as PAIRING_DEFAULTS
-from mavis.summary.constants import DEFAULTS as SUMMARY_DEFAULTS
+from mavis.schemas import DEFAULTS
 from mavis.util import ENV_VAR_PREFIX
-from mavis.validate.constants import DEFAULTS as VALIDATION_DEFAULTS
 
 
-def generate_settings_doc():
+def json_to_pytype(record):
+    input_type = record
+    try:
+        input_type = record['type']
+    except TypeError:
+        pass
+    types = {'string': 'str', 'integer': 'int', 'float': 'float'}
+
+    if input_type == 'array':
+        try:
+            sub_type = json_to_pytype(record['items']['type'])
+            return f'List[{sub_type}]'
+        except TypeError:
+            return 'List'
+
+    if isinstance(input_type, list):
+        # Union
+        types = ', '.join([json_to_pytype(t) for t in input_type])
+        return f'Union[{types}]'
+    return types.get(input_type, input_type)
+
+
+def generate_settings_doc(schema_file):
+    with open(schema_file, 'r') as fh:
+        schema = json.load(fh)
     dirname = os.path.dirname(os.path.abspath(__file__))
+    filepath = 'configuration/settings.md'
+    title = 'Configurable Settings'
 
-    for (filepath, title, namespaces) in [
-        (
-            'configuration/settings.md',
-            'Configurable Settings',
-            [
-                REFERENCE_DEFAULTS,
-                SUMMARY_DEFAULTS,
-                PAIRING_DEFAULTS,
-                ANNOTATION_DEFAULTS,
-                VALIDATION_DEFAULTS,
-                CLUSTER_DEFAULTS,
-                ILLUSTRATION_DEFAULTS,
-            ],
-        ),
-    ]:
-        fname = os.path.join(dirname, filepath)
-        print('writing:', fname)
-        with open(fname, 'w') as fh:
-            fh.write(f'\n\n# {title}\n')
-            glossary = {}
-            for namespace in namespaces:
-                for term, value in namespace.items():
-                    typ = namespace.type(term).__name__
-                    # typ = CUSTOM_TYPES.get(typ, typ)
-                    desc = re.sub(r"\.?$", "", namespace.define(term, "")).capitalize()
-                    accepted = ''
-                    try:
-                        accepted = '\n\n**accepted values**: {}\n'.format(
-                            ', '.join(['`{}`'.format(repr(v)) for v in namespace.type(term).values()])
-                        )
-                    except AttributeError:
-                        pass
-                    defn = f'''## {term}
+    fname = os.path.join(dirname, filepath)
+    print('writing:', fname)
+    with open(fname, 'w') as fh:
+        fh.write(f'\n\n# {title}\n')
+        glossary = {}
+        for term, defn in schema['properties'].items():
+            if term in ['libraries', 'convert']:
+                continue
+            typ = json_to_pytype(defn)
+            desc = defn.get('description', '')
+            default_value = defn.get('default')
+            schema_defn = json.dumps(
+                {k: v for k, v in defn.items() if k not in ['description', 'default']},
+                sort_keys=True,
+                indent='    ',
+            )
+            schema_defn = f'**schema definition**:\n```json\n{schema_defn}\n```\n'
 
-**type**: `#!python {typ}`
-
-**environment variable**: `{ENV_VAR_PREFIX}{term.upper()}`
-
-**default**: `#!python {repr(value)}`{accepted}
-
-{desc}
-        '''
-                    glossary[term] = defn
-            for term, defn in sorted(glossary.items()):
-                fh.write(f'{defn}\n\n')
+            lines = [
+                f'## {term}',
+                f'**type**: `#!python {typ}`',
+                f'**default**: `#!python {repr(default_value)}`',
+                desc,
+                schema_defn,
+            ]
+            glossary[term] = '\n\n'.join(lines)
+        for term, defn in sorted(glossary.items()):
+            fh.write(f'{defn}\n\n')
 
 
 def build_package_docs(config):
-    generate_settings_doc()
+    schema_file = os.path.join(os.path.dirname(__file__), '../mavis/schemas/config.json')
+    generate_settings_doc(schema_file)
     package_dir = os.path.join(os.path.dirname(__file__), '../mavis')
     output_dir = os.path.join(os.path.dirname(__file__), 'package')
+
     extract_to_markdown(
         [package_dir],
         output_dir,
--- docs/outputs/columns.md
+++ docs/outputs/columns.md
@@ -34,7 +34,7 @@ decision from the annotation step
 
 ## event\_type
 
-**type**: `mavis.constants.SVTYPE`
+**type**: [`mavis.constants.SVTYPE`](/package/mavis/constants/#class-mavisconstantssvtype)
 
 The
 classification of the event
@@ -57,7 +57,7 @@ Gene for the current annotation at the first breakpoint
 
 ## gene1\_direction
 
-**type**: `mavis.constants.PRIME`
+**type**: [`mavis.constants.PRIME`](/package/mavis/constants/#class-mavisconstantsprime)
 
 The
 direction/prime of the gene
@@ -68,7 +68,7 @@ Gene for the current annotation at the second breakpoint
 
 ## gene2\_direction
 
-**type**: `mavis.constants.PRIME`
+**type**: [`mavis.constants.PRIME`](/package/mavis/constants/#class-mavisconstantsprime)
 
 The
 direction/prime of the gene. Has the following possible values
@@ -85,7 +85,7 @@ second breakpoint
 
 ## gene\_product\_type
 
-**type**: `mavis.constants.GENE_PRODUCT_TYPE`
+**type**: [`mavis.constants.GENE_PRODUCT_TYPE`](/package/mavis/constants/#class-mavisconstantsgene_product_type)
 
 Describes if the putative fusion product will be
 sense or anti-sense
@@ -105,7 +105,8 @@ Transcript for the current annotation at the second breakpoint
 
 ## fusion\_splicing\_pattern
 
-`mavis.constants.SPLICE_TYPE` -
+**type**: [`mavis.constants.SPLICE_TYPE`](/package/mavis/constants/#class-mavisconstantsslice_type)
+
 Type of splicing pattern used to create the fusion cDNA.
 
 ## fusion\_cdna\_coding\_start
@@ -205,14 +206,14 @@ End integer inclusive
 
 ## break1\_orientation
 
-**type**: `mavis.constants.ORIENT`
+**type**: [`mavis.constants.ORIENT`](/package/mavis/constants/#class-mavisconstantsorient)
 
 The side
 of the breakpoint wrt the positive/forward strand that is retained.
 
 ## break1\_strand
 
-**type**: `mavis.constants.STRAND`
+**type**: [`mavis.constants.STRAND`](/package/mavis/constants/#class-mavisconstantsstrand)
 
 The
 strand wrt to the reference positive/forward strand at this
@@ -246,14 +247,14 @@ End integer inclusive
 
 ## break2\_orientation
 
-**type**: `mavis.constants.ORIENT`
+**type**: [`mavis.constants.ORIENT`](/package/mavis/constants/#class-mavisconstantsorient)
 
 The side
 of the breakpoint wrt the positive/forward strand that is retained.
 
 ## break2\_strand
 
-**type**: `mavis.constants.STRAND`
+**type**: [`mavis.constants.STRAND`](/package/mavis/constants/#class-mavisconstantsstrand)
 
 The
 strand wrt to the reference positive/forward strand at this
@@ -283,7 +284,8 @@ protocol was strand specific or not. Expects a boolean
 
 ## protocol
 
-`mavis.constants.PROTOCOL` -
+**type**: [`mavis.constants.PROTOCOL`](/package/mavis/constants/#class-mavisconstantsprotocol)
+
 Specifies the type of library
 
 ## tools
@@ -404,7 +406,7 @@ event
 
 ## call\_method
 
-**type**: `mavis.constants.CALL_METHOD`
+**type**: [`mavis.constants.CALL_METHOD`](/package/mavis/constants/#class-mavisconstantscall_method)
 
 The
 method used to call the breakpoints
