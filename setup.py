b3c8558a206810608f6bcd010f6c87bff9c1846b
Jeremy Fan <jfan@bcgsc.ca>
2021-12-22 13:14:12 -0800

    added newlins

--- setup.py
+++ setup.py
@@ -24,9 +24,9 @@ def check_nonpython_dependencies():
     else:
         print('Found: aligner at', pth)
 
-setup(
-    # HSTLIB is a dependency for pysam.
-    # The cram file libraries fail for some OS versions and mavis does not use cram files so we disable these options
-    os.environ['HTSLIB_CONFIGURE_OPTIONS'] = '--disable-lzma --disable-bz2 --disable-libcurl'
-)
+# HSTLIB is a dependency for pysam.
+# The cram file libraries fail for some OS versions and mavis does not use cram files so we disable these options
+os.environ['HTSLIB_CONFIGURE_OPTIONS'] = '--disable-lzma --disable-bz2 --disable-libcurl'
+
+setup()
 check_nonpython_dependencies()
