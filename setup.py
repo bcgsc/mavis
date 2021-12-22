2ffc747c025ff74c4a7be9ba8332f835b20ecabc
Jeremy Fan <jfan@bcgsc.ca>
2021-12-21 22:02:44 -0800

    move licurl

--- setup.py
+++ setup.py
@@ -24,5 +24,9 @@ def check_nonpython_dependencies():
     else:
         print('Found: aligner at', pth)
 
-setup()
+setup(
+    # HSTLIB is a dependency for pysam.
+    # The cram file libraries fail for some OS versions and mavis does not use cram files so we disable these options
+    os.environ['HTSLIB_CONFIGURE_OPTIONS'] = '--disable-lzma --disable-bz2 --disable-libcurl'
+)
 check_nonpython_dependencies()
