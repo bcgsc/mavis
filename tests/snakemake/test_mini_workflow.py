a9abb205c77f8580e3f274b95ed81611bb00da2f
Caralyn Reisle <creisle@bcgsc.ca>
2022-01-20 18:27:28 -0800

    Add test for annotation-only workflow

--- tests/snakemake/test_mini_workflow.py
+++ tests/snakemake/test_mini_workflow.py
@@ -12,6 +12,16 @@ from snakemake import main as snakemake_main
 from ..util import glob_exists, long_running_test, package_relative_file
 
 
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+
+
 @pytest.fixture
 def blat_output_dir():
     temp_output = tempfile.mkdtemp()
@@ -46,6 +56,21 @@ def bwa_output_dir():
 
 
 @pytest.fixture
+def annotate_only_output_dir():
+    temp_output = tempfile.mkdtemp()
+
+    os.makedirs(os.path.join(temp_output, 'mavis/schemas'))
+
+    with open(package_relative_file('tests/mini-tutorial.annotate_only.config.json'), 'r') as fh:
+        config = json.load(fh)
+    config['output_dir'] = os.path.join(temp_output, 'output_dir')
+    with open(os.path.join(temp_output, 'mini-tutorial.config.json'), 'w') as fh:
+        fh.write(json.dumps(config))
+    yield temp_output
+    shutil.rmtree(temp_output)
+
+
+@pytest.fixture
 def output_dir(request):
     return request.getfixturevalue(request.param)
 
@@ -67,22 +92,63 @@ def test_workflow(output_dir):
     with patch.object(sys, 'argv', argv):
         try:
             snakemake_main()
-            assert glob_exists(os.path.join(output_dir, 'summary', 'MAVIS.COMPLETE'))
-            assert glob_exists(os.path.join(output_dir, 'pairing', 'MAVIS.COMPLETE'))
-            assert glob_exists(os.path.join(output_dir, 'mock-A47933', 'cluster', 'MAVIS.COMPLETE'))
-            assert glob_exists(
-                os.path.join(output_dir, 'mock-A47933', 'validate', '*', 'MAVIS.COMPLETE')
-            )
-            assert glob_exists(
-                os.path.join(output_dir, 'mock-A47933', 'annotate', '*', 'MAVIS.COMPLETE')
-            )
-            assert glob_exists(os.path.join(output_dir, 'mock-A36971', 'cluster', 'MAVIS.COMPLETE'))
-            assert glob_exists(
-                os.path.join(output_dir, 'mock-A36971', 'validate', '*', 'MAVIS.COMPLETE')
-            )
-            assert glob_exists(
-                os.path.join(output_dir, 'mock-A36971', 'annotate', '*', 'MAVIS.COMPLETE')
-            )
+
         except SystemExit as err:
             if err.code != 0:
                 raise err
+
+    list_files(output_dir)
+    for expected_file in [
+        os.path.join('summary', 'MAVIS.COMPLETE'),
+        os.path.join('pairing', 'MAVIS.COMPLETE'),
+        os.path.join('mock-A47933', 'cluster', 'MAVIS.COMPLETE'),
+        os.path.join('mock-A47933', 'annotate', 'batch-*', 'MAVIS.COMPLETE'),
+        os.path.join('mock-A36971', 'cluster', 'MAVIS.COMPLETE'),
+        os.path.join('mock-A36971', 'annotate', 'batch-*', 'MAVIS.COMPLETE'),
+        os.path.join('mock-A47933', 'validate', 'batch-*', 'MAVIS.COMPLETE'),
+        os.path.join('mock-A36971', 'validate', 'batch-*', 'MAVIS.COMPLETE'),
+    ]:
+        if not glob_exists(os.path.join(output_dir, 'output_dir', expected_file)):
+            raise AssertionError(f'{expected_file} does not exist')
+
+
+@long_running_test
+@pytest.mark.parametrize('output_dir', ['annotate_only_output_dir'], indirect=True)
+def test_no_validate_worflow(output_dir):
+    argv = [
+        'snakemake',
+        '-s',
+        package_relative_file('Snakefile'),
+        '-j',
+        '1',
+        '--configfile',
+        os.path.join(output_dir, 'mini-tutorial.config.json'),
+        '-d',
+        package_relative_file(),
+    ]
+    with patch.object(sys, 'argv', argv):
+        try:
+            snakemake_main()
+
+        except SystemExit as err:
+            if err.code != 0:
+                raise err
+
+    list_files(output_dir)
+    for expected_file in [
+        os.path.join('summary', 'MAVIS.COMPLETE'),
+        os.path.join('pairing', 'MAVIS.COMPLETE'),
+        os.path.join('mock-A47933', 'cluster', 'MAVIS.COMPLETE'),
+        os.path.join('mock-A47933', 'annotate', 'batch-*', 'MAVIS.COMPLETE'),
+        os.path.join('mock-A36971', 'cluster', 'MAVIS.COMPLETE'),
+        os.path.join('mock-A36971', 'annotate', 'batch-*', 'MAVIS.COMPLETE'),
+    ]:
+        if not glob_exists(os.path.join(output_dir, 'output_dir', expected_file)):
+            raise AssertionError(f'{expected_file} does not exist')
+
+    for unexpected_file in [
+        os.path.join('mock-A47933', 'validate', 'batch-*', 'MAVIS.COMPLETE'),
+        os.path.join('mock-A36971', 'validate', 'batch-*', 'MAVIS.COMPLETE'),
+    ]:
+        if glob_exists(os.path.join(output_dir, 'output_dir', unexpected_file)):
+            raise AssertionError(f'{unexpected_file} exists')
