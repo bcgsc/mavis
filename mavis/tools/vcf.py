34840cf518d08b274d097454f6b7600dd9981859
Caleb Choo <cchoo@bcgsc.ca>
2023-02-13 14:15:24 -0800

    adding support for gridss vcf files

--- mavis/tools/vcf.py
+++ mavis/tools/vcf.py
@@ -25,53 +25,55 @@ def parse_bnd_alt(alt):
     | [p[ur        | RR      |
     | ]p]ur        | RL      |
     | ru]p]        | LL      |
+    | u.           |         |
+    | .u           |         |
     """
     # ru[p[
-    match = re.match(r'^(?P<ref>\w)(?P<useq>\w*)\[(?P<chr>[^:]+):(?P<pos>\d+)\[$', alt)
+    match = re.match(r"^(?P<ref>\w)(?P<useq>\w*)\[(?P<chr>[^:]+):(?P<pos>\d+)\[$", alt)
     if match:
         return (
-            match.group('chr'),
-            int(match.group('pos')),
+            match.group("chr"),
+            int(match.group("pos")),
             ORIENT.LEFT,
             ORIENT.RIGHT,
-            match.group('ref'),
-            match.group('useq'),
+            match.group("ref"),
+            match.group("useq"),
         )
     # [p[ur
-    match = re.match(r'^\[(?P<chr>[^:]+):(?P<pos>\d+)\[(?P<useq>\w*)(?P<ref>\w)$', alt)
+    match = re.match(r"^\[(?P<chr>[^:]+):(?P<pos>\d+)\[(?P<useq>\w*)(?P<ref>\w)$", alt)
     if match:
         return (
-            match.group('chr'),
-            int(match.group('pos')),
+            match.group("chr"),
+            int(match.group("pos")),
             ORIENT.RIGHT,
             ORIENT.RIGHT,
-            match.group('ref'),
-            match.group('useq'),
+            match.group("ref"),
+            match.group("useq"),
         )
     # ]p]ur
-    match = re.match(r'^\](?P<chr>[^:]+):(?P<pos>\d+)\](?P<useq>\w*)(?P<ref>\w)$', alt)
+    match = re.match(r"^\](?P<chr>[^:]+):(?P<pos>\d+)\](?P<useq>\w*)(?P<ref>\w)$", alt)
     if match:
         return (
-            match.group('chr'),
-            int(match.group('pos')),
+            match.group("chr"),
+            int(match.group("pos")),
             ORIENT.RIGHT,
             ORIENT.LEFT,
-            match.group('ref'),
-            match.group('useq'),
+            match.group("ref"),
+            match.group("useq"),
         )
     # ru]p]
-    match = re.match(r'^(?P<ref>\w)(?P<useq>\w*)\](?P<chr>[^:]+):(?P<pos>\d+)\]$', alt)
+    match = re.match(r"^(?P<ref>\w)(?P<useq>\w*)\](?P<chr>[^:]+):(?P<pos>\d+)\]$", alt)
     if match:
         return (
-            match.group('chr'),
-            int(match.group('pos')),
+            match.group("chr"),
+            int(match.group("pos")),
             ORIENT.LEFT,
             ORIENT.LEFT,
-            match.group('ref'),
-            match.group('useq'),
+            match.group("ref"),
+            match.group("useq"),
         )
     else:
-        raise NotImplementedError('alt specification in unexpected format', alt)
+        raise NotImplementedError("alt specification in unexpected format", alt)
 
 
 def convert_record(record, record_mapping={}, log=DEVNULL):
@@ -94,7 +96,7 @@ def convert_record(record, record_mapping={}, log=DEVNULL):
             try:
                 value = record.info[key]
             except UnicodeDecodeError as err:
-                log('Ignoring invalid INFO field {} with error: {}'.format(key, err))
+                log("Ignoring invalid INFO field {} with error: {}".format(key, err))
             else:
                 try:
                     value = value[0] if len(value) == 1 else value
@@ -103,27 +105,45 @@ def convert_record(record, record_mapping={}, log=DEVNULL):
             info[key] = value
 
         std_row = {}
-        if record.id and record.id != 'N':  # to account for NovoBreak N in the ID field
-            std_row['id'] = record.id
+        if record.id and record.id != "N":  # to account for NovoBreak N in the ID field
+            std_row["id"] = record.id
 
-        if info.get('SVTYPE', None) == 'BND':
-            chr2, end, orient1, orient2, ref, alt = parse_bnd_alt(alt)
-            std_row[COLUMNS.break1_orientation] = orient1
-            std_row[COLUMNS.break2_orientation] = orient2
-            std_row[COLUMNS.untemplated_seq] = alt
-            if record.ref != ref:
-                raise AssertionError(
-                    'Expected the ref specification in the vcf record to match the sequence '
-                    'in the alt string: {} vs {}'.format(record.ref, ref)
-                )
+        if info.get("SVTYPE", None) == "BND":
+            if "." in alt:
+                # ru.
+                match = re.match(r"^(?P<ref>\w)(?P<useq>\w*)\.$", alt)
+                if match:
+                    chr2 = record.chrom
+                    std_row[COLUMNS.break1_orientation] = ORIENT.NS
+                    std_row[COLUMNS.break2_orientation] = ORIENT.NS
+                    std_row[COLUMNS.untemplated_seq] = match.group("useq")
+                # .ur
+                match = re.match(r"^\.(?P<useq>\w*)(?P<ref>\w)$", alt)
+                if match:
+                    chr2 = record.chrom
+                    std_row[COLUMNS.break1_orientation] = ORIENT.NS
+                    std_row[COLUMNS.break2_orientation] = ORIENT.NS
+                    std_row[COLUMNS.untemplated_seq] = match.group("useq")
+                end = record.stop
+
+            else:
+                chr2, end, orient1, orient2, ref, alt = parse_bnd_alt(alt)
+                std_row[COLUMNS.break1_orientation] = orient1
+                std_row[COLUMNS.break2_orientation] = orient2
+                std_row[COLUMNS.untemplated_seq] = alt
+                if record.ref != ref:
+                    raise AssertionError(
+                        "Expected the ref specification in the vcf record to match the sequence "
+                        "in the alt string: {} vs {}".format(record.ref, ref)
+                    )
         else:
-            chr2 = info.get('CHR2', record.chrom)
+            chr2 = info.get("CHR2", record.chrom)
             end = record.stop
             if (
                 alt
                 and record.ref
-                and re.match(r'^[A-Z]+$', alt)
-                and re.match(r'^[A-Z]+', record.ref)
+                and re.match(r"^[A-Z]+$", alt)
+                and re.match(r"^[A-Z]+", record.ref)
             ):
                 std_row[COLUMNS.untemplated_seq] = alt[1:]
                 size = len(alt) - len(record.ref)
@@ -133,7 +153,7 @@ def convert_record(record, record_mapping={}, log=DEVNULL):
                     std_row[COLUMNS.event_type] = SVTYPE.DEL
         std_row.update({COLUMNS.break1_chromosome: record.chrom, COLUMNS.break2_chromosome: chr2})
         if info.get(
-            'PRECISE', False
+            "PRECISE", False
         ):  # DELLY CI only apply when split reads were not used to refine the breakpoint which is then flagged
             std_row.update(
                 {
@@ -147,26 +167,26 @@ def convert_record(record, record_mapping={}, log=DEVNULL):
             std_row.update(
                 {
                     COLUMNS.break1_position_start: max(
-                        1, record.pos + info.get('CIPOS', (0, 0))[0]
+                        1, record.pos + info.get("CIPOS", (0, 0))[0]
                     ),
-                    COLUMNS.break1_position_end: record.pos + info.get('CIPOS', (0, 0))[1],
-                    COLUMNS.break2_position_start: max(1, end + info.get('CIEND', (0, 0))[0]),
-                    COLUMNS.break2_position_end: end + info.get('CIEND', (0, 0))[1],
+                    COLUMNS.break1_position_end: record.pos + info.get("CIPOS", (0, 0))[1],
+                    COLUMNS.break2_position_start: max(1, end + info.get("CIEND", (0, 0))[0]),
+                    COLUMNS.break2_position_end: end + info.get("CIEND", (0, 0))[1],
                 }
             )
 
-        if 'SVTYPE' in info:
-            std_row[COLUMNS.event_type] = info['SVTYPE']
+        if "SVTYPE" in info:
+            std_row[COLUMNS.event_type] = info["SVTYPE"]
 
         try:
-            orient1, orient2 = info['CT'].split('to')
-            connection_type = {'3': ORIENT.LEFT, '5': ORIENT.RIGHT, 'N': ORIENT.NS}
+            orient1, orient2 = info["CT"].split("to")
+            connection_type = {"3": ORIENT.LEFT, "5": ORIENT.RIGHT, "N": ORIENT.NS}
             std_row[COLUMNS.break1_orientation] = connection_type[orient1]
             std_row[COLUMNS.break2_orientation] = connection_type[orient2]
         except KeyError:
             pass
         std_row.update(
-            {k: v for k, v in info.items() if k not in {'CHR2', 'SVTYPE', 'CIPOS', 'CIEND', 'CT'}}
+            {k: v for k, v in info.items() if k not in {"CHR2", "SVTYPE", "CIPOS", "CIEND", "CT"}}
         )
         records.append(std_row)
     return records
@@ -185,7 +205,7 @@ def convert_file(input_file: str, file_type: str, log):
     rows = []
     vfile = VariantFile(input_file)
     try:
-        vfile.header.info.add('END', number=1, type='Integer', description='End of the interval')
+        vfile.header.info.add("END", number=1, type="Integer", description="End of the interval")
     except ValueError:
         pass
 
@@ -196,5 +216,5 @@ def convert_file(input_file: str, file_type: str, log):
             if file_type not in [SUPPORTED_TOOL.STRELKA, SUPPORTED_TOOL.MUTECT]:
                 raise err
             else:
-                log('Ignoring', vcf_record)
+                log("Ignoring", vcf_record)
     return rows
